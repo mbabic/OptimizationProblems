7769d7ed714b79d5e80069eb18ef5b8db980bfcc
mbabic <mbabic@ualberta.ca>
2014-05-26 08:24:13 -0600

    Graph coloring datasets and runners added.

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -1,48 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+from subprocess import Popen, PIPE
+
 
 def solveIt(inputData):
-    # Modify this code to run your optimization algorithm
 
-    # parse the input
-    lines = inputData.split('\n')
+    # Write input data to temp file.
+    tmpFileName = 'tmp.data'
+    tmpFile = open(tmpFileName, 'w')
+    tmpFile.write(inputData)
+    tmpFile.close()
 
-    firstLine = lines[0].split()
-    items = int(firstLine[0])
-    capacity = int(firstLine[1])
+    # Invoke compiled solver.
+    process = Popen(['bin/knapsack_solver', tmpFileName],
+                    stdout=PIPE)
+    (stdout, stderr) = process.communicate()
 
-    values = []
-    weights = []
+    # Remove temp file
+    os.remove(tmpFileName)
 
-    for i in range(1, items+1):
-        line = lines[i]
-        parts = line.split()
-
-        values.append(int(parts[0]))
-        weights.append(int(parts[1]))
-
-    items = len(values)
-
-    # a trivial greedy algorithm for filling the knapsack
-    # it takes items in-order until the knapsack is full
-    value = 0
-    weight = 0
-    taken = []
-
-    for i in range(0, items):
-        if weight + weights[i] <= capacity:
-            taken.append(1)
-            value += values[i]
-            weight += weights[i]
-        else:
-            taken.append(0)
-
-    # prepare the solution in the specified output format
-    outputData = str(value) + ' ' + str(0) + '\n'
-    outputData += ' '.join(map(str, taken))
-    return outputData
-
+    return stdout.strip()
 
 import sys
 
